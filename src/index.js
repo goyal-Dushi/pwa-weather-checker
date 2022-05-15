bbc5a6d02c90c51876efd52923bc3543486e8bf4
goyal-Dushi <dushyantgoyal28@gmail.com>
2022-05-15 16:25:10 +0530

    installed aframe dependencies and included them

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
